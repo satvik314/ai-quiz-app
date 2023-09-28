1802f4b7dca1afec946988fd8246ccb09ec54d82
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-09-28 11:22:07 +0530

    Removed comments

--- page6.py
+++ page6.py
@@ -15,73 +15,21 @@ def app():
         session_state.score = 0
     title_placeholder = st.empty()
     subheader = st.empty()
-    # board_placeholder = st.empty()
-    # class_placeholder = st.empty()
-    # subject_placeholder = st.empty()
-    # lesson_placeholder = st.empty()
-    # button2 = st.empty()
     topic_placeholder = st.empty()
     n_placeholder = st.empty()
     standard_placeholder = st.empty()
-    # button = st.empty()
 
     with open('Syllabus1.json', 'r') as f:
         data = json.load(f)
 
-    # if stud.get_board() == None or stud.get_lesson() == None or stud.get_std() == None or stud.get_subject() == None:
     title_placeholder.title("Welcome to AI Quiz Generator!")
     subheader.subheader("Test your knowledge on your choice of topic.")
-    # board_names = [board["name"] for board in data["boards"]]
-    # board = board_placeholder.selectbox("select board which you are studying",board_names)
-    # board = board_placeholder.text_input("Enter the board in which you are studying")
-    # board_list = next((b for b in data["boards"] if b["name"] == board), None)
-
-    # class_names = [Class["name"] for Class in board_list["classes"]]
-    # class_names = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII']
-    # classe = class_placeholder.selectbox("Select class in which you are studying", class_names)
-    # classe_list= next((b for b in board_list["classes"] if b["name"]==classe) , None)
-
-    # subject_names = [subject["name"] for subject in classe_list["subjects"]]
-    # subject = subject_placeholder.selectbox("Select subject which you want to study now",subject_names)
-    # subject = subject_placeholder.text_input("Enter the subject which you want to study!")
-    # subject_list= next((b for b in classe_list["subjects"] if b["name"]==subject) , None)
-
-    # lesson_names = [lesson["name"] for lesson in subject_list["lessons"]]
-    # lesson = lesson_placeholder.selectbox("Select Lesson which you want to study now ",lesson_names)
-    # lesson = lesson_placeholder.text_input("Enter lesson which you want to study.")
 
     topic = topic_placeholder.text_input("Enter the topic")
 
     standard = standard_placeholder.selectbox("select the standard", ["Basic", "Intermediate", "Advanced"])
     n = n_placeholder.number_input("Number of questions", min_value=1, max_value=25, value=1, step=1)
 
-    # lesson_list= next((b for b in subject_list["lessons"] if b["name"]==lesson) , None)
-    # if button2.button("Enter"):
-    # stud.set_board(board)
-    # stud.set_std(classe)
-    # stud.set_subject(subject)
-    # stud.set_lesson(lesson)
-    #     board_placeholder.empty()
-    #     class_placeholder.empty()
-    #     subject_placeholder.empty()
-    #     lesson_placeholder.empty()
-    #     topic_placeholder.empty()
-    #     standard_placeholder.empty()
-    #     button2.empty()
-# if stud.get_board() != None and stud.get_lesson() != None and stud.get_std() != None and stud.get_subject() != None:
-    # title_placeholder.header(f"You are studying {stud.get_lesson()} chapter")
-    # st.subheader(f"Where do you want to test your knowledge")
-    # board_list = next((b for b in data["boards"] if b["name"] == stud.get_board()), None)
-    # classe_list= next((b for b in board_list["classes"] if b["name"] == stud.get_std()) , None)
-    # subject_list= next((b for b in classe_list["subjects"] if b["name"] == stud.get_subject()) , None)
-    # lesson_list= next((b for b in subject_list["lessons"] if b["name"] == stud.get_lesson()) , None)
-
-    # st.sidebar.metric(label="Board", value=stud.get_board())
-    # st.sidebar.metric(label="Standard", value=stud.get_std())
-
-    # topic = topic_placeholder.text_input("Enter the topic")
-    # standard = standard_placeholder.selectbox("select the standard", ["Basic", "Intermediate", "Advanced"])
-    # n = n_placeholder.number_input("Number of questions", min_value=1, max_value=25, value=1, step=1)
     if st.button("Generate"):
         st.empty()
         st.empty()
